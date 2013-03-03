88d3b852502153a4589a22138d73605dadc22bf4
doobiest <doobiest@doobiest.net>
2013-03-03 16:29:53 -0500

    groovy

--- autoblinds.ino
+++ autoblinds.ino
@@ -4,86 +4,98 @@
 //This script can also be used with a tmp36 temperature sensor to tilt up when it's very hot out
 //Currently it will tilt up when it's very bright out, but ideally we want this do be done by temperature.
 //Written by Ssshake - Contact arduinocode@doobiest.net for help or to submit contributes to the code line.
-
-#include <Servo.h> 
- 
+#include <Servo.h>  
 Servo myservo;  // create servo object to control a servo 
                 // a maximum of eight servo objects can be created 
 int pos = 0;    // variable to store the servo position 
 int dest = 0;   // Servo destination depending on photocell reading
 int spd = 50;   // how fast should the servo move? 50 is quier
 
-
+int servoPin=0; //havent used this yet
 int photocellPin = 0; // the cell and 10K pulldown are connected to a0
 int photocellReading; // the analog reading from the analog resistor divider
 
 int state = 0; //Keep track of state so we don't send signal to the servo without readon, better on battery life
 int prevstate = 0;
 
+int debug = 1; //Set this to 1 for serial debug output
 void setup(void) {
-      Serial.begin(9600); //Open servial for debug, disable this in headless operation
+    Serial.begin(9600); //Comment out this line if you don't want debugging enabled
 }
 
 
 void loop(void) {
 
     photocellReading = analogRead(photocellPin); //Query photo cell
-    Serial.print("Analog reading = ");
-    Serial.print(photocellReading); // the raw analog reading
-    
+      debug and Serial.print("Light Reading :");
+      debug and Serial.print(photocellReading); // the raw analog reading
+      debug and Serial.print(" | Position: ");
+      debug and Serial.print(pos);    
+      debug and Serial.print(" | State: ");
 
     //Define the modes based on how bright it is, and set corresponding servo position
-    if (photocellReading < 300) {
-      Serial.println(" - Night");
+    if (photocellReading < 400) {
+        debug and Serial.println("Night");
       dest=0;      
       state=1;
     } 
-    else if (photocellReading < 400) {
-      Serial.println(" - Dusk");
+    else if (photocellReading < 500) {
+        debug and Serial.println("Dusk");
       dest=45;      
       state=2;           
     } 
     else if (photocellReading < 700) {
-      Serial.println(" - Day");
-      dest=90;
+        debug and Serial.println("Day");
+      dest=95;
       state=3;
       
     } 
     else if (photocellReading < 1023) {
-      Serial.println(" - Very Hot out");
+        debug and Serial.println("Very Hot out");
       dest=160;
       state=4;
-      
     } 
     else {
-      Serial.println(" - No reading");      
+        debug and Serial.println(" - No reading");      
     }
     
 
 
     if (state != prevstate){ //IF the photocell reading is different from last sample then execute servo controls
-      Serial.println(" - State Change");      
+          debug and Serial.println("State Change");      
+        myservo.attach(9);                     //Connect to servo
         if (pos > dest){  // If the current position is great than the destination then we must subtract
-            myservo.attach(9);                     //Connect to servo
-            for(pos = pos; pos>=dest; pos-=1)     // Change current position to desired position, one degree at a time.
-            {                                
-              myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-              delay(spd);                       // waits 15ms for the servo to reach the position 
-            } 
+              //for(pos = pos; pos>=dest; pos-=1)     // Change current position to desired position, one degree at a time.
+              while (pos > dest)
+              {                                
+                  debug and Serial.print("Was :");
+                  debug and Serial.print(pos);
+                myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+                delay(spd);                 // waits 15ms for the servo to reach the position 
+                pos--;
+                  debug and Serial.print(" | Is :");
+                  debug and Serial.println(pos);
+              } 
             myservo.detach();                //Detach from Servo
-        }
-        
+        } 
         else {  // If the current position is great than the destination then we must add
             myservo.attach(9);
-            for(pos = pos; pos < dest; pos+=1)     // goes from 180 degrees to 0 degrees 
-            {                                
-              myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-              delay(spd);                       // waits 15ms for the servo to reach the position 
-            } 
-            myservo.detach();
+              //for(pos = pos; pos <= dest; pos+=1)     // goes from 180 degrees to 0 degrees 
+              while (pos < dest)
+              {                        
+                  debug and Serial.print("Was :");
+                  debug and Serial.print(pos);        
+                myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+                delay(spd);                     // waits 15ms for the servo to reach the position 
+                pos++;
+                  debug and Serial.print(" | Is :");
+                  debug and Serial.println(pos);
+              } 
         }
+        myservo.write(pos);
+        delay(spd);
+        myservo.detach();
     }
     prevstate = state; //Remember state so we can compare it again next round
-    delay(1000); //Optional delay, this probalby needs to be removed when IR receiver code get's added.
-    
+    delay(1000); //Optional delay, this probalby needs to be removed when IR receiver code get's added.    
 }
