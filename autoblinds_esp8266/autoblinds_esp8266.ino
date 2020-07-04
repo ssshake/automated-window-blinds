08faf1dfa21af818ada41783fee44a88c391fd53
Richard Bettridge <daggasoft@gmail.com>
2020-07-03 20:40:44 -0400

    over the air updates

--- autoblinds_esp8266/autoblinds_esp8266.ino
+++ autoblinds_esp8266/autoblinds_esp8266.ino
@@ -1,8 +1,12 @@
 #include <Servo.h>  
-#include <ESP8266WiFi.h>
 
-const char* ssid = "asdf";
-const char* password = "asdf";
+#include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library (you most likely already have this in your sketch)
+
+#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+
+#include <ArduinoOTA.h>
 
 WiFiServer server(80);
 
@@ -21,20 +25,74 @@ int dest = 0;   // Servo destination depending on photocell reading
 
 int debug = 0; //Set this to 1 for serial debug output
 
+void loop(void) {
+    
+    //handleBlindAutomation();
+    handleWebClients();
+    ArduinoOTA.handle(); //port 8266
+}
+
 void setup(void) {
+    WiFiManager wifiManager;
+    wifiManager.autoConnect("HomeAwesomation");
     Serial.begin(9600); //Comment out this line if you don't want debugging enabled    
     myservo.attach(servoPin);
     startWebServer();
+
+    
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+
+  //ota
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void startWebServer(){
-    // Connect to WiFi network
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
- 
-  WiFi.begin(ssid, password);
  
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -148,19 +206,12 @@ void handleBlindAutomation(){
     }
     
     if (state != prevstate){
-      ldr_press();
+      handleLightChange();
     }
     prevstate = state;
 }
 
-
-void loop(void) {
-    
-		//handleBlindAutomation();
-    handleWebClients();
-}
-
-void ldr_press() { //This executes when the photocell readings have dictated
+void handleLightChange() { //This executes when the photocell readings have dictated
 	  debug and Serial.println("State Change");      
 	myservo.attach(servoPin);                     //Connect to servo
 	if (pos > dest){  // If the current position is great than the destination then we must subtract
@@ -194,7 +245,4 @@ void ldr_press() { //This executes when the photocell readings have dictated
 	myservo.detach(); //Detach servo to keep it from humming on strain
 }
 
-
-
-  
   
